1db5335fafb9430f4e9ec0a071c3148d6fe58a27
Maitena Achigar <maitenaachigar@gmail.com>
2026-09-10 17:36:16 -0300

    Contenido al html, estilos para el css y mas cosas
    Agregue contenido al html, tanto texto como imagenes. Aplique estilos al CSS y hice el primer ejercicio de js sobre los Datos random del artista

---
+++ js/datosCuriosos.js
@@ -0,0 +1,11 @@
+//DATOS CURIOSOS
+//Datos
+let datos = ["Rafael Lozano-Hemmer es un artista mexicano-canadiense especializado en instalaciones interactivas en espacios públicos.","Su obra combina tecnología avanzada como sensores biométricos, robótica y datos en tiempo real.","En su proyecto Vectorial Elevation, ciudadanos controlaban reflectores gigantes a través de internet.","Pulse Room es una instalación que traduce los latidos cardíacos de los visitantes en pulsos de luz.","Su trabajo explora la relación entre el cuerpo humano, la tecnología y la vigilancia.","Ha expuesto sus obras en más de 70 países alrededor del mundo.","Lozano-Hemmer estudió ingeniería antes de dedicarse al arte, lo que influye en su enfoque tecnológico.","Sus instalaciones suelen involucrar la participación activa del público para activar la obra.","Fue ganador del prestigioso premio Ars Electronica por su innovador uso de tecnología en arte.","Utiliza la luz como lenguaje poético para explorar temas de identidad, memoria y presencia."];
+let selecionador = document.querySelector('#selecionadorDatos');
+let mostrar = document.querySelector('#mostrarDato');
+//FUNCION
+//La funcion se ejecuta cuando se hace click en el boton y se muestra en la pagina
+selecionador.addEventListener ('click', function(){
+    let datoSelecionador = Math.floor(Math.random()* datos.length)
+    mostrar.innerText = datos[datoSelecionador];
+})
---
+++ js/galeria.js
@@ -0,0 +1,8 @@
+//Array de obejetos que contenga las obras de arte, el nombre y el año de la misma
+let galeria = [];
+//variables
+let mostrar = document.querySelector('#galeriaArte)');
+//Mostrar 
+for (let i = 0; i < galeria.length; i++){
+
+}
