3325e11f1f2db72a8ffb1acacc0c67a089aaf988
Maitena Achigar <maitenaachigar@gmail.com>
2026-09-11 20:13:23 -0300

    Inicio del ejercicio JS
    Hice el formulario para el ejercicio de JS y cree variables para guardar las respuestas de el formulario en estas mismas variables.

---
+++ js/ejercicio.js
@@ -0,0 +1,16 @@
+let infoObras = []; // Array de obejetos para guardar de cada obra: su nombre, la cantidad de luces moviles que utiliza y el tiempo de funcionamiento por día en horas
+//Variables para guardar los datos del formulario
+let cantidadObras = document.querySelector('#cantidadObras');//Variable para guardar la cantidad de obras que se deben desarrollar
+let obraNombre = document.querySelector('#obraNombre');
+let tiempoPerHora = document.querySelector('#tiempoPerHora');
+//Variables consumo 
+let consumoPerHoraluz = document.querySelector('#consumoPerHoraluz');
+let costoKwh = document.querySelector('#costoKwh');
+//Botones variables
+let enviarInfo = document.querySelector('#botonFormEnviar');
+let borrarInfo = document.querySelector('#botonFormBorrar');
+//Necesito una funcion que haga un push al array 
+/*
+infoObras.push({
+    nombre: obraNombre.push
+})*/
